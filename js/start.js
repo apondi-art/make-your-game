1f607a47ed55c71910a1af30b0ca3c0aa5c3de3e
quochieng <aiyanarich64@gmail.com>
2025-03-12 12:58:42 +0300

    reamove unneccesary spaces

--- js/start.js
+++ js/start.js
@@ -1,11 +1,15 @@
+
 export function initStartButton(startCallback) {
     const startOverlay = document.getElementById('startOverlay');
     const startBtn = document.getElementById('startBtn');
     const pauseBtn = document.getElementById('pauseBtn');
+    
 
     startBtn.addEventListener('click', () => {
         startOverlay.style.display = 'none';
         pauseBtn.style.display = 'block';
+    
+
         
         if (startCallback) {
             startCallback(); 
